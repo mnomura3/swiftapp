f36a99dcb6e3b768ed4e9ce87df331743f2656a2
Masayoshi.Nomura <masayoshi.nomura1983@gmail.com>
2020-05-09 19:16:10 +0900

    Initial Commit

---
+++ swiftapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftapp
+//
+//  Created by masa on 2020/05/09.
+//  Copyright © 2020 Masayoshi Nomura. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
